6751223cfd3d32223d2fba9a7693ad1e333d8764
victor <washingtin@sina.com>
2016-08-15 15:33:37 +0800

    修改       添加dictDao注解

--- src/main/java/com/washingtin/jeenet/modules/sys/dao/DictDao.java
+++ src/main/java/com/washingtin/jeenet/modules/sys/dao/DictDao.java
@@ -1,6 +1,7 @@
 package com.washingtin.jeenet.modules.sys.dao;
 
 import com.washingtin.jeenet.common.persistence.CrudDao;
+import com.washingtin.jeenet.common.persistence.annotation.MyBatisDao;
 import com.washingtin.jeenet.modules.sys.entity.Dict;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
  * 描述：字典DAO接口
  * @author jeenet
  */
+@MyBatisDao
 public interface DictDao extends CrudDao<Dict>{
     public List<String> findTypeList(Dict dict);
 }
